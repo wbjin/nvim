426b9bf2769ef493c69fbb86dce014a2daac1ffb
Wonbin Jin <wbjin@umich.edu>
2024-05-12 21:35:58 -0400

    multi cursor

---
+++ lua/plugins/multicursor.lua
@@ -0,0 +1,17 @@
+return {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'smoka7/hydra.nvim',
+    },
+    opts = {},
+    c = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+        {
+            mode = { 'v', 'n' },
+            '<Leader>m',
+            '<cmd>MCstart<cr>',
+            desc = 'Create a selection for selected text or word under the cursor',
+        },
+    },
+}
---
+++ lua/plugins/sync.lua
@@ -0,0 +1,8 @@
+return {
+    -- "wbjin/sync-nvim",
+    dir="/Users/wonbinjin/Desktop/projects/sync",
+    lazy=false,
+    config = function()
+        require("sync").setup()
+    end,
+}
